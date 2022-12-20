b600e374c844e104a9e14990c2cf9cf4c843c67a
emryu <eunmiryu9@fnfcorp.com>
2022-12-20 14:21:13 +0900

    20221209 baekjoon 10828 스택

--- codingTest/src/codingTestQueStack/BaekJoon10828.java
+++ codingTest/src/codingTestQueStack/BaekJoon10828.java
@@ -5,7 +5,7 @@ import java.util.Scanner;
 public class BaekJoon10828 {
 	
 	public static int[] stack;
-	public static int size = 0;
+	public static int size = 0; 
 	
 	public static void main(String[] args) {
 		Scanner in = new Scanner(System.in);
